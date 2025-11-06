7354795742be850ec2cce21879137c3e2afe3bef
roku133 <69042004+roku133@users.noreply.github.com>
2025-11-06 15:08:30 +0100

    3rd update test_weather13.py
    delete raw test case (before pattern swapping)

--- tests/protocols/test_weather13.py
+++ tests/protocols/test_weather13.py
@@ -15,24 +15,18 @@ logging.basicConfig(level=logging.DEBUG)
 class TestWeather13(unittest.TestCase):
     def test_decode_1(self) -> None:
         decoded = weather13.decode(
-            "01020102020102020202020202020202010202010102010201010101020102020101020103"
-        )
-        self.assertDictEqual({"id": 164, "unit": 1, "temperature": 15.4, "humidity": 77, "lowBattery": False}, decoded)
-
-    def test_decode_2(self) -> None:
-        decoded = weather13.decode(
             "02010201010201010101010101010101020101020201020102020202010201010202010203"
         )
         self.assertDictEqual({"id": 164, "unit": 1, "temperature": 15.4, "humidity": 77, "lowBattery": False}, decoded)
 
         
-    def test_decode_3(self) -> None:
+    def test_decode_2(self) -> None:
         decoded = weather13.decode(
             "02010201010201010101010101010101010201020201020102020202010201020101010103"
         )
         self.assertDictEqual({"id": 164, "unit": 1, "temperature": 9.0, "humidity": 80, "lowBattery": False}, decoded)
         
-    def test_decode_4(self) -> None:
+    def test_decode_3(self) -> None:
         decoded = weather13.decode(
             "02020202010101020201020101010101020202010202010202020202010102020101010103"
         )
