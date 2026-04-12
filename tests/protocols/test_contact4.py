7c207feb2a770c20098bfb2fa5c5f5b92bca9bf2
cvoinf <schieder@166315.nrw.schule>
2026-04-12 14:10:49 +0200

    Update close-test

--- tests/protocols/test_contact4.py
+++ tests/protocols/test_contact4.py
@@ -2,6 +2,11 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
+# pylint: disable=line-too-long
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import logging
 import unittest
 
@@ -12,16 +17,13 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class TestContact4(unittest.TestCase):
-    def test_decode_open(self) -> None:
-        # First signal (contact opened). Expect id 609760, contact True, lowBattery False
-        decoded = contact4.decode("01101001100110100101100101010110101010100101011012")
+    def test_decode_open_1(self) -> None:
+        decoded = contact4.decode("01101001100110100101100101010110101010100110011012")
         self.assertDictEqual({"id": 609760, "contact": True, "lowBattery": False}, decoded)
 
-    def test_decode_close(self) -> None:
-        # Second signal (contact closed). Expect same id, contact False, lowBattery False
-        decoded = contact4.decode("0110101010100101011012")
+    def test_decode_close_1(self) -> None:
+        decoded = contact4.decode("01101001100110100101100101010110101010100101011012")
         self.assertDictEqual({"id": 609760, "contact": False, "lowBattery": False}, decoded)
-
 
 if __name__ == "__main__":
     unittest.main()
